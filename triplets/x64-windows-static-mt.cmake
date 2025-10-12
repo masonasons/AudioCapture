8900b56c14647cbf2af2093451de2a9ef812f747
Bri <briexists@gmail.com>
2025-10-12 18:37:05 -0500

    Add custom vcpkg triplet and update CI workflow

---
+++ triplets/x64-windows-static-mt.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_PLATFORM_TOOLSET_VERSION 143)
+
+# Use static C/C++ runtime (/MT and /MTd)
+set(VCPKG_CRT_LINKAGE static)
